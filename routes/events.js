901a167a7ac32cea5cde60c0cb80811634a753b0
dunndealpro <dunn.evan.f@gmail.com>
2023-01-20 06:54:27 -0800

    update selected venue page

--- routes/events.js
+++ routes/events.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var eventsCtrl = require('../controllers/events')
 const isLoggedIn = require('../config/auth')
 
+router.get('/events/:id/delete', isLoggedIn, eventsCtrl.deleteEvent)
 router.get('/events', eventsCtrl.index);
 router.get('/events/new', isLoggedIn, eventsCtrl.new);
 router.get('/events/:id', isLoggedIn, eventsCtrl.show);
@@ -13,6 +14,5 @@ router.get('/events/:id/:id1', isLoggedIn, eventsCtrl.detailed)
 // router.post('/events/:id/worker', isLoggedIn, workersCtrl.addToEvent);
 // router.get('/events/:id/edit', isLoggedIn, eventsCtrl.editeventDetails);
 router.put('/events/:id', isLoggedIn, eventsCtrl.updateEvent);
-router.delete('/events/:id', isLoggedIn, eventsCtrl.deleteEvent)
 
 module.exports = router
